c58e9ff7afbd7bac8e1e3a8ac16bf6e523730ee0
Matres <matthijsalderliefste@hotmail.com>
2015-10-31 11:16:13 +0100

    Skeleton intergration with Grav

---
+++ cache/compiled/files/1ac666a3758b880f62878183ebda66c7.yaml.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'user/config/site.yaml',
+    'modified' => 1446215924,
+    'data' => [
+        'title' => 'Indoicle',
+        'author' => [
+            'name' => 'Matthijs Alderliefste',
+            'email' => 'm@mateis.nl'
+        ],
+        'metadata' => [
+            'description' => 'A ‘Fast-food content’ website for the Indonesian market'
+        ]
+    ]
+];
---
+++ cache/compiled/files/68d9899361ef18c42c23d2c9c70eac98.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'themes://antimatter/antimatter.yaml',
+    'modified' => 1446217895,
+    'data' => [
+        'enabled' => true,
+        'dropdown' => [
+            'enabled' => true
+        ],
+        'streams' => [
+            'schemes' => [
+                'theme' => [
+                    'type' => 'ReadOnlyStream',
+                    'paths' => [
+                        0 => 'user/themes/antimatter'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
